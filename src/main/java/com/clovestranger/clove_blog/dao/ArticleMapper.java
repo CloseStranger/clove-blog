ff77bd6b9a15de0d013fdd18f322f320de72720b
Ziang Zhu <ziang_zhu@thinkyeah.com>
2023-06-06 17:54:15 +0800

    add mybatis

--- src/main/java/com/clovestranger/clove_blog/dao/ArticleMapper.java
+++ src/main/java/com/clovestranger/clove_blog/dao/ArticleMapper.java
@@ -1,9 +1,11 @@
 package com.clovestranger.clove_blog.dao;
 
 import com.clovestranger.clove_blog.model.Article;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface ArticleMapper {
     List<Article> articleLists();
 }
