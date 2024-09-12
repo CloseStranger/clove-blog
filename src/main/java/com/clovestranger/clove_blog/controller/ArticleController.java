b5cfb542a8b5e7399157580bb8f8d65e6583bdf0
dcdev_mini <dcdev_flutter@dcdev-minideMac-mini.local>
2024-09-12 18:25:57 +0800

    update

--- src/main/java/com/clovestranger/clove_blog/controller/ArticleController.java
+++ src/main/java/com/clovestranger/clove_blog/controller/ArticleController.java
@@ -1,12 +1,19 @@
 package com.clovestranger.clove_blog.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.clovestranger.clove_blog.common.CommonResult;
 import com.clovestranger.clove_blog.model.Article;
 import com.clovestranger.clove_blog.service.ArticleService;
-import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("/article")
@@ -14,7 +21,6 @@ public class ArticleController {
 
     @Resource
     private ArticleService articleService;
-
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
--- src/main/java/com/clovestranger/clove_blog/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/clovestranger/clove_blog/service/impl/ArticleServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.sql.Timestamp;
 import java.util.*;
 
-
 @Service
 public class ArticleServiceImpl implements ArticleService {
     @Resource
@@ -45,20 +44,22 @@ public class ArticleServiceImpl implements ArticleService {
         List<Article> dbInfo = articleMapper.articleLists(article);
         if (tagIds != null) {
             List<Article> output = new ArrayList<>();
-//            String[] filterTags = Arrays.stream(tagIds.split(",")).sorted().toArray(String[]::new);
-//            dbInfo.forEach(item -> {
-//                String[] itemTags = item.getTags().stream().map(x -> Integer.toString(x.getId())).sorted().toArray(String[]::new);
-//                if (filterTags.length > itemTags.length) {
-//                    return;
-//                }
-//                boolean pushSignal = true;
-//                for (String filterTag : filterTags) {
-//                    pushSignal = pushSignal && (Arrays.binarySearch(itemTags, filterTag) != -1);
-//                }
-//                if (pushSignal) {
-//                    output.add(item);
-//                }
-//            });
+            // String[] filterTags =
+            // Arrays.stream(tagIds.split(",")).sorted().toArray(String[]::new);
+            // dbInfo.forEach(item -> {
+            // String[] itemTags = item.getTags().stream().map(x ->
+            // Integer.toString(x.getId())).sorted().toArray(String[]::new);
+            // if (filterTags.length > itemTags.length) {
+            // return;
+            // }
+            // boolean pushSignal = true;
+            // for (String filterTag : filterTags) {
+            // pushSignal = pushSignal && (Arrays.binarySearch(itemTags, filterTag) != -1);
+            // }
+            // if (pushSignal) {
+            // output.add(item);
+            // }
+            // });
             List<String> filterTags = Arrays.stream(tagIds.split(",")).sorted().toList();
             dbInfo.forEach(item -> {
                 List<String> itemTags = item.getTags().stream().map(x -> Integer.toString(x.getId())).toList();
